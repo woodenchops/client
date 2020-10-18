428b89cf03c154f7719eeb3c2be1485d440152d6
Kristian Woods <kris_woods@hotmail.co.uk>
2020-10-18 12:31:05 +0100

    fc

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import List from './components/List';
+import EditListItem from './components/EditListIem'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    editMode: false,
+    items: [
+      {
+        name: 'Bread',
+        price: 1.90
+      },
+      {
+        name: 'Milk',
+        price: 1.90
+      }
+    ]
+  }
+
+  saveItemName = (value, id) => {
+    this.setState({
+      items: [...this.state.items.map((x, indx) =>  {
+        return {
+          ...x, 
+         name: (indx === id) ? value : x.name
+        }
+      })],
+      editMode: false
+    });
+  }
+
+  editItem = () => {
+    this.setState({
+      editMode: true
+    })
+  }
+
+  render() {
+    const {items, editMode} = this.state
+    return (
+      <div className="App">
+        <h1>hello</h1>
+        <ul>
+          { (items) && (items.map((item, idx) => (
+              (!editMode) ? <List key={idx} name={item.name} itemId={idx} editItem={this.editItem}/> : (<EditListItem key={idx} name={item.name} itemId={idx} saveItemName={this.saveItemName}/>)
+          ))) }
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
